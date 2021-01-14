463ba15b8ef83905fcbd8d1d373f3b7c014f2f4a
iamgroot <73430776+amfisevan@users.noreply.github.com>
2021-01-14 20:20:31 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@
       function checkPassword() {
         var password = document.getElementById("passwordbox");
         var passwordText = password.value;
-        if(passwordText == "eunbinfool") {
-          return true;
-        }
-        alert("Access denied")
-        return false;
+            if(passwordText == "eunbinfool") {
+              return true;
+            }
+            alert("Access denied")
+            return false;
       }
     </script>
