31d063350ed337ed3e46c6d3e53728eae1088c3f
iamgroot <73430776+amfisevan@users.noreply.github.com>
2021-01-14 19:55:10 +0900

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+    <script>
+      function checkPassword() {
+        var password = document.getElementById("passwordbox");
+        var passwordText = password.value;
+        if(passwordText == "eunbinfool") {
+          return true;
+        }
+        alert("Access denied")
+        return false;
+      }
+    </script>
